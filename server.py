8fc559232494e8823642ecd733259a5914a1e1e8
kilzsfaja <kevin_k_gautier@hotmail.com>
2023-04-15 17:19:32 -0700

    added edit, show and delete functionality

--- server.py
+++ server.py
@@ -1,35 +1,61 @@
-from flask import Flask, render_template, request, redirect
-from users import User
+from flask import Flask, render_template, request, redirect, url_for
+from users_model import User
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("create.html")
+    return redirect("/users_all")
 
-# @app.route("/")
-# def index():
-#     users = User.get_all()
-#     print(users)
-#     return render_template("read.html", all_users = users)
-
-@app.route('/read', methods=['GET'])
-def show():
+# ------------- DISPLAY ALL USERS -------------
+@app.route('/users_all', methods=['GET'])
+def users():
     users = User.get_all()
-    return render_template("read.html", all_users = users)
+    return render_template("users_all.html", all_users = users)
 
-@app.route('/create/user', methods=["POST"])
+# -------------- CREATE USER (render) --------------
+@app.route('/users/new')
+def new_user():
+    return render_template("create_user.html")
+
+# --------------- CREATE USER (action) -----------------
+@app.route('/users/create', methods=["POST"])
 def create_user():
-    # First we make a data dictionary from our request.form coming from our template.
-    # The keys in data need to line up exactly with the variables in our query string.
     data = {
-        "first_name": request.form["first_name"],
-        "last_name" : request.form["last_name"],
-        "email" : request.form["email"]
+        **request.form
     }
-    User.save(data)
-    # Don't forget to redirect after saving to the database.
-    return redirect('/read')
+    user_id = User.save(data)
+    return redirect(url_for('show_user', id = user_id))
+# why did URL FOR work for this but not for UPDATE route? Other ways??
+
+# ----------------- DISPLAY ONE USER ------------------
+@app.route('/users/show/<int:id>')
+def show_user(id):
+    user = User.get_one(id)
+    return render_template("show_user.html", user = user)
+
+# ------------------- EDIT USER (render) --------------------
+@app.route('/users/edit/<int:id>')
+def edit_user(id):
+    user = User.get_one(id)
+    return render_template("edit_user.html", user = user)
+
+# ----------------- EDIT USER (action) -----------------
+@app.route('/users/update/<int:id>', methods=["POST"])
+def update_user(id):
+    data = {
+        **request.form,
+        "id" : id
+    }
+    User.edit_user(data)
+    return redirect('/')
+    # return redirect(url_for('show_user', id = id))
+
+# ------------------- DELETE USER ------------------------
+@app.route('/users/delete/<int:id>')
+def delete(id):
+    User.delete(id)
+    return redirect('/')
 
 # @app.route("/users/read/form/<int:id>", methods=["GET"])
 # def display_read(id):
@@ -42,8 +68,8 @@ def create_user():
 if __name__ == "__main__":
     app.run(debug=True)
 
-# **********************
 
+# **********************
 #   Method: GET
 #   grabbing everything in a list
 #   URL: make it plural ex: "/todos"
